b0be75f9e0d19952c06c20bffb166e7d284e2e55
Yura <yurii_kulachenko@mail.ru>
2020-04-08 01:26:43 +0300

    блок с картой (карта не меняет иконку при изменеии размера и центрирование) мобилка планшет десктоп

---
+++ source/js/yandex-map.js
@@ -0,0 +1,28 @@
+ymaps.ready(init);
+
+let iconSize = [60, 50];
+
+function init() {
+  var map = new ymaps.Map("my-map", {
+    center: [59.93886466, 30.32310321],
+    zoom: 17,
+    controls: [],
+    // behaviors: ["drag"],
+  });
+
+  var point = new ymaps.Placemark([59.93886466, 30.32310321],
+    {
+      hintContent: "Магазин Кэт энерджи",
+    },
+    {
+      iconLayout: "default#image",
+      iconImageHref: "/img/map-pin.png",
+      iconImageSize: iconSize,
+      iconImageOffset: [-30, -30],
+    });
+  map.geoObjects.add(point);
+
+  map.events.add('resize', function () {
+    console.log('О, событие!');
+  });
+}
